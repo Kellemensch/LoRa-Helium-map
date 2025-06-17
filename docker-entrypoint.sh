2104e24b2a398db4f12753998533e40e379bd783
DEMOISY BAPTISTE <noreply@example.com>
2025-06-17 11:08:42 +0200

    Build version

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,30 +4,5 @@ set -e
 # Activer le venv
 source venv/bin/activate
 
-# echo "🔧 Initialisation de la configuration..."
-
-# # Lire latitude et longitude s'ils ne sont pas déjà définis
-# if [ ! -f configs/.latitude ]; then
-#   read -p "Latitude de votre end-node (ex: 45.70377): " latitude
-#   echo "$latitude" > configs/.latitude
-# else
-#   echo "✅ Latitude déjà définie : $(cat configs/.latitude)"
-# fi
-
-# if [ ! -f configs/.longitude ]; then
-#   read -p "Longitude de votre end-node (ex: 13.7204): " longitude
-#   echo "$longitude" > configs/.longitude
-# else
-#   echo "✅ Longitude déjà définie : $(cat configs/.longitude)"
-# fi
-
-# # Lire le subdomain si absent
-# if [ ! -f configs/.subdomain ]; then
-#   read -p "Nom du subdomain localtunnel : " subdomain
-#   echo "$subdomain" > configs/.subdomain
-# else
-#   echo "✅ Subdomain déjà défini : $(cat configs/.subdomain)"
-# fi
-
-echo "🚀 Lancement de l'application..."
+echo "🚀 Launching application..."
 python3 main.py --logs
--- run.sh
+++ run.sh
@@ -1,40 +1,46 @@
 #!/bin/bash
 
-echo "🔧 Initialisation de la configuration..."
+IMAGE_NAME="lora-helium-map"
+CONTAINER_NAME="lora-map"
 
-# Lire latitude et longitude s'ils ne sont pas déjà définis
-if [ ! -f configs/.latitude ]; then
-  read -p "Latitude de votre end-node (ex: 45.70377): " latitude
-  echo "$latitude" > configs/.latitude
-else
-  echo "✅ Latitude déjà définie : $(cat configs/.latitude)"
-fi
+echo "🔧 Initializing configuration..."
 
-if [ ! -f configs/.longitude ]; then
-  read -p "Longitude de votre end-node (ex: 13.7204): " longitude
-  echo "$longitude" > configs/.longitude
-else
-  echo "✅ Longitude déjà définie : $(cat configs/.longitude)"
-fi
+# # Lire latitude et longitude s'ils ne sont pas déjà définis
+# if [ ! -f configs/.latitude ]; then
+#   read -p "Latitude of the end-node (ex: 45.70377): " latitude
+#   echo "$latitude" > configs/.latitude
+# else
+#   echo "✅ Latitude already defined : $(cat configs/.latitude)"
+# fi
 
-# Lire le subdomain si absent
-if [ ! -f configs/.subdomain ]; then
-  read -p "Nom du subdomain localtunnel : " subdomain
-  echo "$subdomain" > configs/.subdomain
-else
-  echo "✅ Subdomain déjà défini : $(cat configs/.subdomain)"
-fi
+# if [ ! -f configs/.longitude ]; then
+#   read -p "Longitude of the end-node (ex: 13.7204): " longitude
+#   echo "$longitude" > configs/.longitude
+# else
+#   echo "✅ Longitude already defined : $(cat configs/.longitude)"
+# fi
+
+# # Lire le subdomain si absent
+# if [ ! -f configs/.subdomain ]; then
+#   read -p "Subdomain name for localtunnel : " subdomain
+#   echo "$subdomain" > configs/.subdomain
+# else
+#   echo "✅ Subdomain already defined : $(cat configs/.subdomain)"
+# fi
 
 UID_LOCAL=$(id -u)
 GID_LOCAL=$(id -g)
 
-docker build -t lora-helium-map .
+docker build -t "$IMAGE_NAME" .
 
-docker run -d --rm \
-  -p 5000:5000 \
-  -v "$(pwd)/output:/app/output" \
-  -e HOST_UID=$UID_LOCAL \
-  -e HOST_GID=$GID_LOCAL \
-  --dns 8.8.8.8 \
-  --name lora-map \
-  lora-helium-map
+# docker run -d --rm \
+#   -p 5000:5000 \
+#   -v "$(pwd)/output:/app/output" \
+#   -e HOST_UID=$UID_LOCAL \
+#   -e HOST_GID=$GID_LOCAL \
+#   --dns 8.8.8.8 \
+#   --name "$CONTAINER_NAME" \
+#   "$IMAGE_NAME"
+
+echo "Logs can be seen with : docker logs -f $CONTAINER_NAME"
+echo "🛑 Stop with : docker stop $CONTAINER_NAME"
