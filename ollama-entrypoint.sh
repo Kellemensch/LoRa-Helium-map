7e01acd5e199b7870002f3c5807eaa6e015925c3
Kellemensch <baptistedemoisy@gmail.com>
2025-06-26 17:20:50 +0200

    final version docker (only with terrain data from here)

--- ollama-entrypoint.sh
+++ ollama-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Démarrer le serveur Ollama en arrière-plan
 ollama serve &
@@ -10,8 +10,12 @@ until curl -s http://localhost:11434 > /dev/null; do
 done
 
 # Télécharger le modèle souhaité
-echo "Downmoading AI model llama3..."
-ollama pull llama3
+if ! ollama list | grep -q "llama3"; then
+    echo "Downloading AI model llama3..."
+    ollama pull llama3
+else
+    echo "Model llama3 already present, skipping pull."
+fi
 
 # Garder le serveur au premier plan (important pour Docker)
 wait
